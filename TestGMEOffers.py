8d1fc0734a4e3ebce135d227c45f065efeba8c22
Michele Nan <michele.nan@ark-energy.eu>
2020-04-16 12:35:45 +0200

    removed test baseaddress and APiKey

--- TestGMEOffers.py
+++ TestGMEOffers.py
@@ -1,7 +1,7 @@
 from Artesian import GMEPublicOfferService
 from Artesian import ArtesianConfig
 
-cfg = ArtesianConfig("https://test-arkive-proxy-abijnkebhgdtw.azurewebsites.net/ArkTest","NP16E00ikgbfCATUgTxWoE4-3BA2xO8PmQmfMuUqCPuk10PPllxeo9TIX3DCz-xVjM4xazIy-GUYWTICxwQLdenp9dHVqS2DeYBp8e1yd-fhp6yDuAQDul3ElP08ryHs")
+cfg = ArtesianConfig("baseaddr","apikey")
 
 qs = GMEPublicOfferService(cfg)
 
--- TestMarketDataService.py
+++ TestMarketDataService.py
@@ -2,7 +2,7 @@ from Artesian import MarketDataService
 from Artesian import RelativeInterval
 from Artesian import ArtesianConfig
 
-cfg = ArtesianConfig("https://test-arkive-proxy-abijnkebhgdtw.azurewebsites.net/ArkTest","NP16E00ikgbfCATUgTxWoE4-3BA2xO8PmQmfMuUqCPuk10PPllxeo9TIX3DCz-xVjM4xazIy-GUYWTICxwQLdenp9dHVqS2DeYBp8e1yd-fhp6yDuAQDul3ElP08ryHs")
+cfg = ArtesianConfig("baseaddr","apikey")
 
 qs = MarketDataService(cfg)
 
