ed15f6e36fa8cdc2881e68bb6d9b32e7d9a92dc3
Niall Mac Dermott <niall.macdermott@ark-energy.eu>
2021-01-11 10:17:38 +0000

    Patch Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="artesian-sdk",
-    version="2.2.0",
+    version="2.2.1",
     author="Ark-Lab",
     long_description = long_description,
     long_description_content_type='text/markdown',
