23c502df452982a0e7f0ff0345cc326793dbd246
AlexandraGref <alexandra.gref@ark-energy.eu>
2022-02-01 09:34:18 +0100

    Import changes:  ._Services.Enum.

--- Artesian/__init__.py
+++ Artesian/__init__.py
@@ -5,14 +5,14 @@ from Artesian._Configuration.ArtesianConfig import ArtesianConfig
 from Artesian._Configuration.ArtesianPolicyConfig import ArtesianPolicyConfig
 from Artesian._Services.Dto.MarketDataService import MarketDataService
 from Artesian._Services.GMEPublicOfferService import GMEPublicOfferService
-from Artesian._GMEPublicOffers.Config.Market import Market
-from Artesian._GMEPublicOffers.Config.Purpose import Purpose
-from Artesian._GMEPublicOffers.Config.Scope import Scope
-from Artesian._GMEPublicOffers.Config.Status import Status
-from Artesian._GMEPublicOffers.Config.UnitType import UnitType
-from Artesian._GMEPublicOffers.Config.Zone import Zone
-from Artesian._GMEPublicOffers.Config.GenerationType import GenerationType
-from Artesian._GMEPublicOffers.Config.BaType import BaType
+from Artesian._Services.Enum.Market import Market
+from Artesian._Services.Enum.Purpose import Purpose
+from Artesian._Services.Enum.Scope import Scope
+from Artesian._Services.Enum.Status import Status
+from Artesian._Services.Enum.UnitType import UnitType
+from Artesian._Services.Enum.Zone import Zone
+from Artesian._Services.Enum.GenerationType import GenerationType
+from Artesian._Services.Enum.BaType import BaType
 
 __all__ = ["QueryService", "Granularity", "RelativeInterval", "ArtesianConfig", "ArtesianPolicyConfig", "MarketDataService","GMEPublicOfferService",
            "Market","Purpose","Scope","Status","UnitType","Zone","GenerationType","BaType" ]
