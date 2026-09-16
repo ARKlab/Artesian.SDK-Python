f599e3b6d6c8c11bd669906c4ff50ac547707971
Jacopo Cina <jacopo.cina@ark-energy.eu>
2026-09-16 12:04:23 +0200

    while line

--- src/Artesian/_ClientsExecutor/ArtesianJsonSerializer.py
+++ src/Artesian/_ClientsExecutor/ArtesianJsonSerializer.py
@@ -22,6 +22,7 @@ def __artesianDatetimeSerializer(obj: datetime, **kwargs: Any) -> str:
     ret = obj.isoformat(timespec="seconds")
     return ret
 
+
 def __artesianDatetimeDeserializer(obj: str, *args: Any, **kwargs: Any) -> datetime:
     return parser.isoparse(obj)
 
